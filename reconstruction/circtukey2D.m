aad87ed8ecf2dc9bfa245715bef308f26b732105
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2021-07-15 14:37:10 +0200

    Update

--- reconstruction/circtukey2D.m
+++ reconstruction/circtukey2D.m
@@ -1,4 +1,4 @@
-function output = circtukey2D(dimy,dimx,filterwidth)
+function output = circtukey2D(dimy,dimx,row,col,filterwidth)
 
 % 2D Tukey filter
 
@@ -8,16 +8,21 @@ base = zeros(domain,domain);
 tukey1 = tukeywin(domain,filterwidth);
 tukey1 = tukey1(domain/2+1:domain);
 
-x = linspace(-domain/2, domain/2, domain);
+shifty = (row-dimy/2)*domain/dimy;
+shiftx = (col-dimx/2)*domain/dimx;
+
 y = linspace(-domain/2, domain/2, domain);
+x = linspace(-domain/2, domain/2, domain);
 
 for i=1:domain
     
     for j=1:domain
         
-        if (round(sqrt(x(i)^2 + y(j)^2)) <= domain/2)
+        rad = round(sqrt((shiftx-x(i))^2 + (shifty-y(j))^2)); 
+        
+        if (rad <= domain/2) && (rad > 0)
             
-            base(i,j) = tukey1(round(sqrt(x(i)^2 + y(j)^2)));
+            base(j,i) = tukey1(rad);
             
         end
         
