6ca0b50cbed4425930a01efba699a7f6b7c1d6d6
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2021-09-02 15:36:17 +0200

    registration bug fix

--- registration/register_images.m
+++ registration/register_images.m
@@ -1,27 +1,27 @@
 function images_out = register_images(app,images_in)
 
 
-[ne,ns,~,~] = size(images_in);
+[ne,~,~,ns] = size(images_in);
 
 [optimizer, metric] = imregconfig('multimodal');
 
-norm = ne*(ns-1);
+norm = ns*(ne-1);
 
-for i = 1:ne
-    
-    for j = 2:ns
+for j = 1:ns
+
+    for i = 2:ne
         
-        image0 = squeeze(images_in(1,j,:,:));
-        image1 = squeeze(images_in(i,j,:,:));
+        image0 = squeeze(images_in(1,:,:,j));
+        image1 = squeeze(images_in(i,:,:,j));
         
         max0 = max(image0(:));
         max1 = max(image1(:));
         
         [image2] = imregister(image1/max1,image0/max0,'similarity',optimizer, metric,'DisplayOptimization',0);
         
-        images_in(i,j,:,:) = image2*max1;
+        images_in(i,:,:,j) = image2*max1;
         
-        app.RegProgressGauge.Value = round(100*((i-1)*(ns-1) + (j-1))/norm);
+        app.RegProgressGauge.Value = round(100*((j-1)*(ne-1) + (i-1))/norm);
         drawnow;
         
     end
