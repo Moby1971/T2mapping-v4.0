8dded37a597f1a3f5255f2709532e031d1b3fd7a
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2022-08-01 13:19:24 +0200

    Version 3.0

--- fitting/dothemobaT2starfit.m
+++ fitting/dothemobaT2starfit.m
@@ -1,8 +1,8 @@
-function [m0MapOut, t2MapOut] = dothemobaT2fit(app, slice)
+function [m0MapOut, t2MapOut] = dothemobaT2starfit(app, slice)
 
-% -----------------------------------------------------------------------
-% Performs a model-based T2 map fitting of multi-echo data for 1 slice
-% -----------------------------------------------------------------------
+% ---------------------------------------------------------------------------------
+% Performs a model-based T2* map fitting of multi-gradient-echo data for 1 slice
+% ---------------------------------------------------------------------------------
 
 
 % Multicoil data
@@ -42,35 +42,44 @@ kSpacePics = permute(kSpace,[6 ,3 ,4 ,1 ,7 ,2 ,8 ,9 ,10,11,12,13,14,5 ]);
 
 
 % Prepare the echo times matrix
-TE(1,1,1,1:app.nrCoils,1,:) = tes*0.001;
+% In the test files in Bart the TE's are mulitplied by 0.01, not 0.001
+% There seems to be a scaling factor of 10
+TE(1,1,1,1:app.nrCoils,1,:) = tes*0.01;
 
-
+% ---------------------
 % Moba reco
-picscommand = 'moba -F -l1 -rS:0 -rT:38:0:0.001 --kfilter-1';
+% ---------------------
+% -G    = T2* mapping using model-based multiple gradient echo
+% -m3   = MGRE model R2S (I assume this means: R2*)
+% -rQ:1 = l2 regularization
+% -rS:0 = non-negative constraint
+%
+picscommand = 'moba -G -m2 -rQ:1 -rS:0 -rW:3:64:1 -i10 -C100 -u0.0001 --kfilter-2 ';
+
+picscommand = 'moba -G -m1 ';
 t2FitCoils = bart(app,picscommand,kSpacePics,TE);
 
 
 % Sum of squares reconstruction over the coil dimension
-t2Fit = abs(bart(app,'rss 16', t2FitCoils));
+t2Fit = bart(app,'rss 16', t2FitCoils);
 
+disp(size(t2Fit))
 
 % Extract M0 map
 m0MapOut = flip(squeeze(t2Fit(1,:,:,1,1,1,1)),2);
 
 
-% Extract T2 map
-% Somewhere in the function T2fun.c of Bart I noticed a scaling factor of 10
-% The T2 value is therefore calculated as: 100/R2 instead of 1000/R2
-t2MapOut = 100./flip(squeeze(t2Fit(1,:,:,1,1,1,2)),2);
+% Extract T2 map in ms
+t2MapOut = 1000./flip(squeeze(t2Fit(1,:,:,1,1,1,2)),2);
 t2MapOut(isinf(t2MapOut)) = 0;
 t2MapOut(isnan(t2MapOut)) = 0;
 
 
 % Remove outliers
-t2MapOut(t2MapOut < 1) = 0;
-t2MapOut(t2MapOut > 5000) = 0;
-m0MapOut(t2MapOut < 1) = 0;
-m0MapOut(t2MapOut > 5000) = 0;
+t2MapOut(t2MapOut < 0.1) = 0;
+t2MapOut(t2MapOut > 500) = 0;
+m0MapOut(t2MapOut < 0.1) = 0;
+m0MapOut(t2MapOut > 500) = 0;
 
 
 % Masking
