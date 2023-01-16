de35a4aa602b9987fc9c43fceb2f25657d5e8d8c
Gustav Strijkers <g.j.strijkers@amsterdamumc.nl>
2023-01-16 06:39:50 +0100

    Maintenance

--- export/export_dicom_t2_dcm.m
+++ export/export_dicom_t2_dcm.m
@@ -28,31 +28,30 @@ dimr = parameters.NO_ECHOES; % number of echoes
 flist = dir(fullfile(dcm_files_path,'*.dcm'));
 files = sort({flist.name});
 
-
 % Generate new dicom headers
 for dynamic = 1:dimd
 
     for slice = 1:dimz
 
         % Read the Dicom header
-        dcm_header(slice,dynamic) = dicominfo([dcm_files_path,filesep,files{ (dynamic-1)*dimz*dimr + (slice-1)*dimr + 1 }]); %#ok<*AGROW>
+        dcm_header{slice,dynamic} = dicominfo([dcm_files_path,filesep,files{ (dynamic-1)*dimz*dimr + (slice-1)*dimr + 1 }]); %#ok<*AGROW>
 
         % Changes some tags
-        dcm_header(slice,dynamic).ImageType = 'DERIVED\RELAXATION\';
-        dcm_header(slice,dynamic).InstitutionName = 'Amsterdam UMC';
-        dcm_header(slice,dynamic).InstitutionAddress = 'Amsterdam, Netherlands';
-        dcm_header(slice,dynamic).TemporalPositionIdentifier = dynamic;
-        dcm_header(slice,dynamic).NumberOfTemporalPositions = dimd;
-        dcm_header(slice,dynamic).TemporalResolution = parameters.TotalAcquisitionTime/dimd;
+        dcm_header{slice,dynamic}.ImageType = 'DERIVED\RELAXATION\';
+        dcm_header{slice,dynamic}.InstitutionName = 'Amsterdam UMC';
+        dcm_header{slice,dynamic}.InstitutionAddress = 'Amsterdam, Netherlands';
+        dcm_header{slice,dynamic}.TemporalPositionIdentifier = dynamic;
+        dcm_header{slice,dynamic}.NumberOfTemporalPositions = dimd;
+        dcm_header{slice,dynamic}.TemporalResolution = parameters.TotalAcquisitionTime/dimd;
         
     end
 
 end
 
 % create folders if not exist, and delete folders content
-dir1 = dcm_header(1).PatientID;
+dir1 = dcm_header{1,1}.PatientID;
 dir2 = 'DICOM';
-dir3 = strcat(num2str(dcm_header(1).SeriesNumber),'T2');
+dir3 = strcat(num2str(dcm_header{1,1}.SeriesNumber),'T2');
 dir41 = '1';
 dir42 = '2';
 dir43 = '3';
@@ -82,10 +81,10 @@ for dynamic = 1:dimd
 
     for slice = 1:dimz
 
-        dcm_header(slice,dynamic).ProtocolName = 'T2-map';
-        dcm_header(slice,dynamic).SequenceName = 'T2-map';
-        dcm_header(slice,dynamic).EchoTime = 1.1;
-        dcm_header(slice,dynamic).ImageType = 'DERIVED\RELAXATION\T2';
+        dcm_header{slice,dynamic}.ProtocolName = 'T2-map';
+        dcm_header{slice,dynamic}.SequenceName = 'T2-map';
+        dcm_header{slice,dynamic}.EchoTime = 1.1;
+        dcm_header{slice,dynamic}.ImageType = 'DERIVED\RELAXATION\T2';
 
         fn = ['0000',num2str(slice)];
         fn = fn(size(fn,2)-4:size(fn,2));
@@ -97,7 +96,7 @@ for dynamic = 1:dimd
        
         image = rot90(squeeze(cast(round(t2map(:,:,slice,dynamic)),'uint16')));
 
-        dicomwrite(image, fname, dcm_header(slice,dynamic));
+        dicomwrite(image, fname, dcm_header{slice,dynamic});
 
     end
 
@@ -112,10 +111,10 @@ for dynamic = 1:dimd
 
     for slice=1:dimz
 
-        dcm_header(slice,dynamic).ProtocolName = 'M0-map';
-        dcm_header(slice,dynamic).SequenceName = 'M0-map';
-        dcm_header(slice,dynamic).EchoTime = 1.2;
-        dcm_header(slice,dynamic).ImageType = 'DERIVED\RELAXATION\M0';
+        dcm_header{slice,dynamic}.ProtocolName = 'M0-map';
+        dcm_header{slice,dynamic}.SequenceName = 'M0-map';
+        dcm_header{slice,dynamic}.EchoTime = 1.2;
+        dcm_header{slice,dynamic}.ImageType = 'DERIVED\RELAXATION\M0';
 
         fn = ['0000',num2str(slice)];
         fn = fn(size(fn,2)-4:size(fn,2));
@@ -125,7 +124,7 @@ for dynamic = 1:dimd
         fname = [output_directory2,filesep,'M0-slice',fn,'-dynamic',dn,'.dcm'];
         image = rot90(squeeze(cast(round(m0map(:,:,slice,dynamic)),'uint16')));
     
-        dicomwrite(image, fname, dcm_header(slice,dynamic));
+        dicomwrite(image, fname, dcm_header{slice,dynamic});
 
     end
 
@@ -138,10 +137,10 @@ for dynamic = 1:dimd
 
     for slice=1:dimz
 
-        dcm_header(slice,dynamic).ProtocolName = 'R^2-map';
-        dcm_header(slice,dynamic).SequenceName = 'R^2-map';
-        dcm_header(slice,dynamic).EchoTime = 1.3;
-        dcm_header(slice,dynamic).ImageType = 'DERIVED\RELAXATION\R2';
+        dcm_header{slice,dynamic}.ProtocolName = 'R^2-map';
+        dcm_header{slice,dynamic}.SequenceName = 'R^2-map';
+        dcm_header{slice,dynamic}.EchoTime = 1.3;
+        dcm_header{slice,dynamic}.ImageType = 'DERIVED\RELAXATION\R2';
 
         fn = ['0000',num2str(slice)];
         fn = fn(size(fn,2)-4:size(fn,2));
@@ -151,7 +150,7 @@ for dynamic = 1:dimd
         fname = [output_directory3,filesep,'R2-slice',fn,'-dynamic',dn,'.dcm'];
         image = rot90(squeeze(cast(round(100*r2map(:,:,slice,dynamic)),'uint16')));
      
-        dicomwrite(image, fname, dcm_header(slice,dynamic));
+        dicomwrite(image, fname, dcm_header{slice,dynamic});
 
     end
 
